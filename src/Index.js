36e248a0ea324716e0cc63558a37efecd1e8bc53
FlorBee <buhleflor@gmail.com>
2024-09-29 17:14:02 +0200

    Updated JS and added API.

--- src/Index.js
+++ src/Index.js
@@ -1,9 +1,26 @@
+function refreshWeather(response) {
+  let weatherTemperature = document.querySelector("#temperature");
+  let temperature = Math.round(response.data.temperature.current);
+  let weatherCity = document.querySelector("#city");
+
+  weatherCity.innerHTML = response.data.city;
+  weatherTemperature.innerHTML = `${temperature}`;
+}
+
+function searchCity(city) {
+  let apiKey = "cf31b3ef6b626986604ad601d3at6doe";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function searchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
-  let weatherCity = document.querySelector("#city");
-  weatherCity.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", searchSubmit);
+
+searchCity("Johannesburg");
